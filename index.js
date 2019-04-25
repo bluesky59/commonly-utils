ec2c58f3a904a52a8ac6fe402597f70c576166b9
maqin <1101787440@qq.com>
2019-04-25 17:21:36 +0800

    整理常用工具库

---
+++ index.js
@@ -0,0 +1,15 @@
+// const axios  = require('./lib/axios');
+// const gps  = require('./lib/gps');
+// const jsBridge  = require('./lib/jsBridge');
+// const map  = require('./lib/map');
+const utils  = require('./lib/utils');
+
+// module.exports = {
+//     axios,
+//     gps,
+//     jsBridge,
+//     map,
+//     utils,
+// };
+
+console.log(utils.arrFlat([3,2,1, [4,5,6]]));
